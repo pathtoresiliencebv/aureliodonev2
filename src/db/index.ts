f5a0b68fd8dcf4f686424d8c1848fce4de8dc9f3
Lubomir Georgiev <lubomir.georgiev.georgiev@gmail.com>
2025-10-18 19:37:49 +0300

    Use React.cache when initializing the D1 Drizzle client

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,23 +1,15 @@
 import { drizzle } from "drizzle-orm/d1";
+import { cache } from "react";
 import { getCloudflareContext } from "@opennextjs/cloudflare";
-import type { DrizzleD1Database } from "drizzle-orm/d1";
 
 import * as schema from "./schema";
 
-export let db: DrizzleD1Database<typeof schema> | null = null;
-
-export const getDB = () => {
-  if (db) {
-    return db;
-  }
-
+export const getDB = cache(() => {
   const { env } = getCloudflareContext();
 
   if (!env.NEXT_TAG_CACHE_D1) {
     throw new Error("D1 database not found");
   }
 
-  db = drizzle(env.NEXT_TAG_CACHE_D1, { schema, logger: true });
-
-  return db;
-};
+  return drizzle(env.NEXT_TAG_CACHE_D1, { schema, logger: true });
+});
